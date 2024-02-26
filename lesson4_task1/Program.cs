d22024c792a7717f7a8c7dc080dc49d27ddc5b4e
Asiya Ermolaeva <asiya_ermolaeva@mail.ru>
2024-02-26 21:55:11 +0300

    add a task solution

--- lesson4_task1/Program.cs
+++ lesson4_task1/Program.cs
@@ -10,6 +10,27 @@ namespace lesson4_task1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите целое число N > 0");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+
+            if (N <= 0)
+            {
+                Console.WriteLine("число N - неположительное");
+            }
+            else
+            {
+                for (int i = 1; i <= N; i++)
+                {
+                    sum += 2 * i - 1; ;
+                    Console.Write("{0} ", 2 * i - 1);
+                }
+
+                Console.WriteLine("сумма - {0} ", sum);
+            }
+            
+            Console.Write("Нажмите любую клавишу");
+            Console.ReadKey();
         }
     }
 }
